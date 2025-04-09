165ef7dce27ff4f1880fa30f943be7f65f112462
mhetacc <marco.bello.vi@gmail.com>
2025-04-09 21:42:02 +0300

    other data for 2004,1999 downloaded

--- project/data_builder.py
+++ project/data_builder.py
@@ -1,34 +1,44 @@
 import pandas 
-
-# Step 1: Merge `parties.csv` and `country_codes.csv` on `DIVISION_ID = alpha-2`
-parties = pandas.read_csv('/home/mhetac/Documents/GitHub/data15003/project/data/eu_parliament/2024/parties.csv', sep=';')
-country_codes = pandas.read_csv('/home/mhetac/Documents/GitHub/data15003/project/data/country_codes.csv', sep=';')
-
-# Merge on DIVISION_ID and alpha-2
-parties_with_countries = pandas.merge(parties, country_codes, left_on='DIVISION_ID', right_on='alpha-2', how='left')
-
-parties_with_countries.to_csv('/home/mhetac/Documents/GitHub/data15003/project/data/build/parties_with_countries.csv', index=False)
+import glob
 
 
-# Step 2: Merge the new `parties.csv` with each `result-parties-country.csv` on `ID = PARTY_ID`
-# Assuming there are multiple `result-parties-country.csv` files
-#import glob
-#
-#result_files = glob.glob('/home/mhetac/Documents/GitHub/data15003/project/data/eu_parliament/2024/result-parties-*.csv')
-#merged_results = []
-#
-#for file in result_files:
-#    result_data = pandas.read_csv(file)
-#    merged_result = pandas.merge(parties_with_countries, result_data, left_on='ID', right_on='PARTY_ID')
-#    merged_results.append(merged_result)
-#
-## Combine all merged results into a single DataFrame
-#final_data = pandas.concat(merged_results, ignore_index=True)
-#
-## Step 3: Add a `year` column to all data
-#final_data['year'] = 2024  # Add the current year
-#
-## Save the final dataset to a CSV file
-#final_data.to_csv('/home/mhetac/Documents/GitHub/data15003/project/data/build/2024_dataset.csv', index=False)
-#
-#print("Merged dataset saved to 'final_dataset.csv'")
+def merge_parties_countries_votepercentage(year):
+        # Read csv files, separating by `;`
+    parties = pandas.read_csv(f'/home/mhetac/Documents/GitHub/data15003/project/data/eu_parliament/{year}/parties.csv', sep=';')
+    country_codes = pandas.read_csv('/home/mhetac/Documents/GitHub/data15003/project/data/country_codes.csv', sep=';')
+
+    # Merge `parties.csv` and `country_codes.csv` on `DIVISION_ID = alpha-2`
+    # Make one dataframe with all parties and their country names 
+    parties_with_countries = pandas.merge(parties, country_codes, left_on='DIVISION_ID', right_on='alpha-2', how='left')
+
+    # save to csv for debugging
+    parties_with_countries.to_csv(f'/home/mhetac/Documents/GitHub/data15003/project/data/build/parties_with_countries_{year}.csv', index=False)
+
+
+    # Merge all national parties results results in one csv
+    # Get all files with results for each country
+    list_of_parties = glob.glob(f'/home/mhetac/Documents/GitHub/data15003/project/data/eu_parliament/{year}/results-parties*.csv', recursive=True)
+
+    # Read and store each file into a list of DataFrames
+    parties_df = [pandas.read_csv(party, sep=';') for party in list_of_parties]
+
+    # Concatenate them into a single DataFrame
+    all_parties_merged = pandas.concat(parties_df, ignore_index=True)
+
+    # save to csv for debugging
+    all_parties_merged.to_csv(f'/home/mhetac/Documents/GitHub/data15003/project/data/build/all_parties_merged_{year}.csv', index=False, sep=';')
+
+
+    # Merge parties with election results 
+    # with parties with countries
+    parties_countries_percentage = pandas.merge(parties_with_countries, all_parties_merged, left_on='ID', right_on='PARTY_ID', how='left')
+
+    # save to csv for debugging
+    parties_countries_percentage.to_csv(f'/home/mhetac/Documents/GitHub/data15003/project/data/build/parties_countries_percentage_{year}.csv', index=False, sep=';')
+
+merge_parties_countries_votepercentage(2024)
+
+
+# 2004, 1999 do not have usable data
+# merge_parties_countries_votepercentage(1999)
+
--- project/report.md
+++ project/report.md
@@ -1,14 +1,31 @@
 ## Dataset
 
+### Problems
+
+Unfortunately data before 2004 is not usable, since there is no way to get votes for the single state and also merging parties with the general result is not possible since we miss the `ACRONYM` field in the single state results, which only report the whole group result.\
+Moreover, the percentages relate to the whole assembly seats, not the in-state vote, meaning we cannot use it to gauge party's popularity in their respective state.
+
+
+This can be seen, for example, at [italian results csv](https://github.com/mhetacc/data15003/blob/main/project/data/eu_parliament/2004/results-parties-it.csv): there is no single party code.
+
+The most we could know is that *party X*, e.g., `IT01`, which may be `FI` as in `Forza Italia` got 22 seats in the parliament. Unfortunately
+
+We will need to calculate it based in the seats assigned to the whole country and the seats assigned to each single party.
+
 ### Build Political Dataset
 
 1. merge `parties.csv` and `country_codes.csv` on `DIVISION_ID = alpha-2`
    1. thus obtain table with parties and respective country name
 2. merge new `parties.csv` with each `result-parties-country.csv` on `ID = PARTY_ID`
    1. thus obtain table with name of parties and percentage 
-3. add to all data `year` column
 
 After this we should have a table with all parties names, countries and year of election.
+
+#### For 2004 and 1999
+
+- calculate for each `results-parties*` percentage of votes based on seats taken
+- procede as before
+- add the `year` column since it is not present
 
 ### Country Codes
 
